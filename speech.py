7123a5614d68d6c49a093107caef08d5dae8e802
Shyam Murali <59696444+ShyamMurali@users.noreply.github.com>
2021-03-18 13:58:03 +0530

    Create speech.py

---
+++ speech.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+import webbrowser as wb
+
+r1=sr.Recognizer()
+r2=sr.Recognizer()
+r3= sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('say something now: ')
+    audio = r1.listen(source)
+    text = r1.recognize_google(audio)
+    print(text)
+    print(type(text)) 
+    
+
+
+    
